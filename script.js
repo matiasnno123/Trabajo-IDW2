5a85cec29af03b93b374ba551aa88f243558f118
matiasnno123 <skateboardsj14@gmail.com>
2024-06-04 18:23:25 -0300

    Se arreglo el reloj

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
     var hora = padzero(objetoHora.getHours());
     var minutos = padzero(objetoHora.getMinutes());
     var segundos = padzero(objetoHora.getSeconds());
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var fechaString = currentTime.toLocaleDateString('es-ES', options);
     var tiempostring = hora + ":" + minutos + ":" + segundos;
+    var fechastring = objetoHora.getDate();
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var fechastring = new Date().toLocaleDateString('es-ES', options);
     document.getElementById("tiempo").textContent = tiempostring;
-    
+    document.getElementById("fecha").textContent = fechastring
     setTimeout(actualizarReloj, 1000);
-
 }
 function padzero(numero){
     return numero < 10 ? "0" + numero : numero;
